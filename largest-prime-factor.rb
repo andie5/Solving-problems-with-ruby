6bcee238ec41c42f2df2d8e38e35a0dd72e017de
Andie <Andie@andies-air.home>
2016-08-19 00:17:56 +0100

    largest prime initial commit

--- largest-prime-factor.rb
+++ largest-prime-factor.rb
@@ -3,6 +3,35 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
-def largest_prime_factor
-  
+def largest_prime_factor(n)
+  starting_index = n/2
+  i = starting_index
+  not_prime = false
+  while ((i > 1) && not_prime == false)
+    j = 2
+    not_prime_inner = false
+    while ((j < i) && not_prime_inner == false)
+      if((i%j) === 0)
+        not_prime_inner = true
+      end
+      j+=1
+    end
+    puts "This interim number is #{j} and not prime inner is #{not_prime_inner}"
+    i-=1
+  end
+
+  puts "#{i}"
+  #   j = 2
+  #   # Inner loop to check if number is a divisible by any other number
+  #   while j <= i
+  #     if((i%j) === 0)
+  #       not_prime = true;
+  #       break;
+  #   # Check the next number is prime
+  #   i-=1
+  # end
 end
+
+
+# largest_prime_factor(600851475143)
+largest_prime_factor(20)
