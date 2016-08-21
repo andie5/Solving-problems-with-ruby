2a6ceb8bf621edb0ebbb8ee4fcfc5df20b304e43
andie5 <andieasa@googlemail.com>
2016-08-21 20:48:33 +0100

    Completed largest prime but needs to be more performant

--- largest-prime-factor.rb
+++ largest-prime-factor.rb
@@ -7,23 +7,30 @@ def largest_prime_factor(n)
   starting_index = n/2
   i = starting_index
   not_prime = false
+
   while ((i > 1) && not_prime == false)
     j = 2
     not_prime_inner = false
-    while ((j < i) && not_prime_inner == false)
+    while (j < i)
       if((i%j) === 0)
         not_prime_inner = true
+    # puts "i is #{i} and j is #{j} and not prime inner is #{not_prime_inner}"
+        break
       end
       j+=1
     end
-    puts "This interim number is #{j} and not prime inner is #{not_prime_inner}"
+    # puts "Inner loop on #{i} and result j ended on #{j}"
+
+    # If the number we are on is found to be prime then exit outer loop
+    if(not_prime_inner === false)
+      not_prime = true
+      break
+    end
+
     i-=1
   end
-
-  puts "#{i}"
-  #   # Inner loop to check if number is a divisible by any other number
+  puts "#{i}" 
 end
 
-
-# largest_prime_factor(600851475143)
-largest_prime_factor(20)
+largest_prime_factor(21)
+largest_prime_factor(600851475143)
